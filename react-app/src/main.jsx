3126532defaba92ae4a03e566ae33eee1a115576
hnaji-el <hamidnaji981@gmail.com>
2024-06-29 22:20:15 +0100

    add some react notes

--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// 2. Create a React element
+const element = React.createElement(
+  "h1",
+  {
+    id: "hello",
+    style: { color: "red", fontSize: "70px" },
+  },
+  "Hello World!"
+);
+
+// 3. Render the application
+const container = document.querySelector("#root");
+const root = createRoot(container);
+root.render(element);
